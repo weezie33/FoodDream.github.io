b6cd63c2dc263e01be7214702e8441aba00bad1c
Wezzie33 <corey.webeck@gmail.com>
2019-03-28 13:26:18 -0400

    overwrite

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,32 @@
 // initial testing
 let apikey =  	"&apikey=2ff458e3e36ac3751209b898369ec5bd";
-let url = "http://api.musixmatch.com/ws/1.1/ track.search?q="
-let param = "&country=us"
 
-let baseUrl = "https:\/\/tracking.musixmatch.com\/t1.0\/3vUCAP8440S6Sk7DuqRzasSEUwaXt--FlCVXVneejDfC2unGzEVZ4rj1th-68BgHiVWF2rAFn8t54CRrgLDhh7dMYkql-9ylfgWbhzYRwlXtWNCj6i5CXcHFMH9C31ZkTRW0yCi-FY4kPFpucR2g4motP3PZ4var0wCniwN8_FcHJKBl2A7HK-uqPiV0lu0s7vGEqMIjlKVXPFG27eEhGUHMNUX0vM9fDJ4IenYvwhNRMtcD6FGIm0vbXyIfll9EEqrYmsoslvb3tOv9jftAsQvE_bIoCn37c4NnxZs\/"
+
+
+let param = "&country=us"
+let lyricMatch= "matcher.lyrics.get"
+let lyricsTrack
+
 let tracksearch = "track.search?q="
 $("#add-art").on("click",function(event){
     event.preventDefault();
+    
     let searchInput = $("#art-show").val().trim();
+    let baseUrl = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?format=json&q="+searchInput+"&callback=jsonp&quorum_factor=1&apikey=2ff458e3e36ac3751209b898369ec5bd"
     $("#art-show").val("")
 
 
     $.ajax({
-        // url: 'https://api.musixmatch.com/ws/1.1/track.search?q=drake&apikey=2ff458e3e36ac3751209b898369ec5bd',
-        url: baseUrl+tracksearch+searchInput,
+        url: baseUrl,
         method: "GET",
     }).then(function(response){
-        $("#searchDump").append(response)
+        let parsed = JSON.parse(response);
+        let obj = parsed.message.body;
+        for(let i = 0; i <obj.track_list.length; i++){
+            $("#searchDump").append(JSON.stringify("trackname "+obj.track_list[i].track.track_name+" Artist Name "+obj.track_list[i].track.artist_name ))
+        }
+        
+        console.log(obj);
     })
 
 
