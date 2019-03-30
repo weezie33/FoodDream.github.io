baa1c374d6e1e4e77b9b843a5a067ffa35c2377c
Bryan Villatoro <bvillatoro@ymail.com>
2019-03-29 20:45:22 -0400

    napster api player and basic layout for html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,64 @@
- 
 // initial testing
-$("#add-art").on("click", function (event) {
-    event.preventDefault();
+let apikey =  	"&apikey=2ff458e3e36ac3751209b898369ec5bd";
 
-    let lyricInput = $("#art-show").val().trim();
-    //clue needs an input field with id
-    let clueInput = "";//$("#somthing").val().trim();
-    let baseUrl = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?format=json&q_lyrics=" + lyricInput + "&q_track_artist=" + clueInput + "&callback=jsonp&quorum_factor=.06&f_has_lyrics=1&s_track_rating=desc&apikey=2ff458e3e36ac3751209b898369ec5bd"
+let tracksearch = "track.search?q="
+$("#add-art").on("click",function(event){
+    event.preventDefault();
+    
+    let searchInput = $("#art-show").val().trim();
+    let baseUrl = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?format=json&q="+searchInput+"&callback=jsonp&quorum_factor=1&apikey=2ff458e3e36ac3751209b898369ec5bd"
     $("#art-show").val("")
-    // empty out value of clue input here
+
 
     $.ajax({
         url: baseUrl,
         method: "GET",
-    }).then(function (response) {
+    }).then(function(response){
         let parsed = JSON.parse(response);
-        console.log(parsed);
         let obj = parsed.message.body;
-        $("#searchDump").text("");
-        for (let i = 0; i < obj.track_list.length; i++) {
-
-            var  newDiv = $("<div>");
-           
-            newDiv.append(
-                $("<p>").text("Track Name: " + obj.track_list[i].track.track_name),
-                $("<p>").text("Artist Name: " + obj.track_list[i].track.artist_name),
-            )
-            newDiv.addClass("output");
-            // $("#searchDump").append(JSON.stringify("Track Name " + obj.track_list[i].track.track_name + " Artist Name " + obj.track_list[i].track.artist_name))
-            
-            $("#searchDump").append(newDiv)
+        for(let i = 0; i <obj.track_list.length; i++){
+            $("#searchDump").append(JSON.stringify("trackname "+obj.track_list[i].track.track_name+" Artist Name "+obj.track_list[i].track.artist_name ))
         }
-
+        
         console.log(obj);
     })
 
 
-    console.log(lyricInput);
+    console.log(searchInput);
 })
-$(".output").on ("click", function( event   )   {
-
-    event.preventDefault();
-    
 
 
+const tracksTemplateSource = document.getElementById('tracks-template').innerHTML;
+const tracksTemplate = Handlebars.compile(tracksTemplateSource);
+const $tracks = $('#musicplay');
+
+// $(document).on("click", ".options", function () {
+
+//     let title = $("<div>").attr("title");
+//     let artist = $("<div>").attt("artist");
+// })
 
 
-})
+function firstCall(value) {
+    $.ajax({
+        url: "https://api.napster.com/v2.2/search?apikey=OWI1YTkyYTctMDRjNC00YjgzLWE2ODItYTZkMzFmNzFmMGQy&query=" + value + "&type=track",
+        method: "GET",
+    }).done(function (response) {
+        let id = response.search.order[0];
+        secondCall(id);
+    })
+}
+
+function secondCall(track) {
+    $.ajax({
+        url: "https://api.napster.com/v2.2/tracks/" + track + "?apikey=OWI1YTkyYTctMDRjNC00YjgzLWE2ODItYTZkMzFmNzFmMGQy",
+        method: "GET"
+    }).then(function (response) {
+        $tracks.html(tracksTemplate(response));
+    })
+
+}
+
+firstCall("Say it aint so");
 
 
